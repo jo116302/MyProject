45984826dbc2b6d7cf535a406c9ac54ca90a4f03
Bio <81629923+jo116302@users.noreply.github.com>
2022-01-23 01:40:46 +0900

    controller edit

--- kr.co.myProject/src/main/java/kr/co/myProject/controller/youtube2Controller.java
+++ kr.co.myProject/src/main/java/kr/co/myProject/controller/youtube2Controller.java
@@ -1,10 +1,16 @@
 package kr.co.myProject.controller;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -14,6 +20,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.google.gson.Gson;
